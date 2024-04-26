f31f0557a15435a43cb7bf1bedeb7a9623cd78bb
HumbertoArellano <humbertoyham25@gmail.com>
2024-04-25 17:12:32 -0700

    API backend

--- src/main/java/com/springboot/backend/backendonlinestore/repositories/ProductRepository.java
+++ src/main/java/com/springboot/backend/backendonlinestore/repositories/ProductRepository.java
@@ -2,9 +2,11 @@ package com.springboot.backend.backendonlinestore.repositories;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.springboot.backend.backendonlinestore.entities.Product;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RepositoryRestResource(path = "productos")
 public interface ProductRepository extends CrudRepository<Product, Integer> { 
 }
